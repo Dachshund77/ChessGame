9873e5e2f4ad08d5f1624ce5e912b2981485a3e9
Sven Büchner <sven.buechner.privat@googlemail.com>
2018-12-05 23:10:14 +0100

    Inital MCV setup

---
+++ src/controllers/MainWindowController.java
@@ -0,0 +1,16 @@
+package controllers;
+
+
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+
+
+public class MainWindowController {
+
+    @FXML
+    private Canvas boardLayer;
+
+    public void initialize(){
+        System.out.println("Test");
+    }
+}
