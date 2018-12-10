a7e275687940a521a652a842494ecd33746a0f41
Sven Büchner <sven.buechner.privat@googlemail.com>
2018-12-10 02:52:33 +0100

    Documented (And minor rewrite) Coordinate class

--- src/Logic/Coordinate.java
+++ src/Logic/Coordinate.java
@@ -1,50 +1,79 @@
 package Logic;
 
 import Logic.Boards.ChessBoard;
+import Logic.Boards.Square;
 
+/**
+ * Thees object will be used to store a x and y coordinate on the board.
+ * Note that Squares[x][y] should have a 0,0 value in the top left corner
+ */
 public class Coordinate {
 
 
     private int xCoordinate;
     private int yCoordinate;
 
+    /**
+     * Creates a Coordinate object that keeps two ints as x/y coordinates.
+     * @param xCoordinate int that stores x coordinate.
+     * @param yCoordinate int that stores y coordinate.
+     */
     public Coordinate(int xCoordinate, int yCoordinate){
         this.xCoordinate = xCoordinate;
         this.yCoordinate = yCoordinate;
     }
 
+    /**
+     * Gets the x coordinate.
+     * @return int with the x coordinate.
+     */
     public int getCoordinateX() {
         return xCoordinate;
     }
 
-    public void setCoordinateX(int xCoordinate) {
-        this.xCoordinate = xCoordinate;
-    }
-
+    /**
+     * Gets the y coordinate.
+     * @return int with the y coordinate.
+     */
     public int getCoordinateY() {
         return yCoordinate;
     }
 
-    public void setCoordinateY(int yCoordinate) {
-        this.yCoordinate = yCoordinate;
+    /**
+     * Compares this Coordinate with an object.
+     * @param obj the object we want to compare with
+     * @return true if the x and y fields have the same value
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null){
+            return false;
+        }
+        else if (!Coordinate.class.isAssignableFrom(obj.getClass())){ //If the object can be a Coordinate
+            return false;
+        }
+        Coordinate other = (Coordinate) obj;
+        if (this.getCoordinateY() == other.getCoordinateY() && this.getCoordinateX() == other.getCoordinateX()){
+            return true;
+        }
+        return false;
+
     }
 
-    public boolean equals(Coordinate c){
+    /**
+     * Checks if the coordinates exist on the Chessboard.
+     * @param board chessboard we will check the length of
+     * @return true if the coordinate is valid
+     */
+    public boolean isValidCoordinate(ChessBoard board){
         boolean returnBoolean = false;
-        if (this.xCoordinate == c.getCoordinateX() && this.yCoordinate == c.getCoordinateY()){
-            returnBoolean = true;
-        }
-        return returnBoolean;
-    }
-
-    public boolean isValidCoordinate(ChessBoard board){ //TODO need to be more accurate
-        boolean returnBoolean = true;
-        int boardLength = board.getSquares().length-1; //That should be 7, length it 8
-        if (xCoordinate < 0 || xCoordinate > boardLength){
-            returnBoolean = false;
-        }
-        if (yCoordinate < 0 || yCoordinate > boardLength){
-            returnBoolean = false;
+        Square[][] squares = board.getSquares();
+        int xLength = squares.length-1;
+        if (xCoordinate >= 0 && xCoordinate <= xLength){ //If x is valid
+            int yLength = squares[xCoordinate].length-1;
+            if (yCoordinate >= 0 && yCoordinate <= yLength){ //If y is valid
+                returnBoolean = true;
+            }
         }
         return returnBoolean;
     }
